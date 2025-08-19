920237ec51ee9cfb86938ab898fde8166e01c6d1
DiegoG <diegogaliano2005@gmail.com>
2025-08-18 19:31:23 -0500

    cambios para purebas postman

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import document_type, history, payment_type, predetermined_price
+from .views.history import histories_list, history_create, history_delete
 
 urlpatterns = [
     path('document-types/', document_type.document_types_list, name='document_types_list'),
     path('document-types/new/', document_type.document_type_create, name='document_type_create'),
     path('document-types/<int:pk>/delete/', document_type.document_type_delete, name='document_type_delete'),
 
-    path('histories/', history.histories_list, name='histories_list'),
-    path('histories/new/', history.history_create, name='history_create'),
-    path('histories/<int:pk>/delete/', history.history_delete, name='history_delete'),
+    path('histories/', histories_list, name='histories_list'),         
+    path('histories/new/', history_create, name='history_create'),      
+    path('histories/<int:pk>/delete/', history_delete, name='history_delete'),
+
 
     path('payment-types/', payment_type.payment_types_list, name='payment_types_list'),
     path('payment-types/new/', payment_type.payment_type_create, name='payment_type_create'),
