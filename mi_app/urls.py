2dfaccbe4d4ec90debd86771d397fdca8495f160
dayana0121 <dayanacarrionespinoza@gmail.com>
2025-08-21 13:48:48 -0500

    Update urls.py

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views.histories import histories_list, history_create, history_update, history_delete
+from .views.history import histories_list, history_create, history_update, history_delete
 from .views.document_types import document_types_list, document_type_create, document_type_update, document_type_delete
 from .views.payment_types import payment_types_list, payment_type_create, payment_type_update, payment_type_delete
 from .views.predetermined_prices import predetermined_prices_list, predetermined_price_create, predetermined_price_update, predetermined_price_delete
 
 urlpatterns = [
-    path("histories/", histories.histories_list, name="histories_list"),
-    path("histories/create/", histories.history_create, name="history_create"),
-    path("histories/<int:pk>/update/", histories.history_update, name="history_update"),
-    path("histories/<int:pk>/delete/", histories.history_delete, name="history_delete"),
+    path("history/", histories.historys_list, name="history_list"),
+    path("history/create/", histories.history_create, name="history_create"),
+    path("history/<int:pk>/update/", histories.history_update, name="history_update"),
+    path("history/<int:pk>/delete/", histories.history_delete, name="history_delete"),
 
     path("document-types/", document_types.document_types_list, name="document_types_list"),
     path("document-types/create/", document_types.document_type_create, name="document_type_create"),
